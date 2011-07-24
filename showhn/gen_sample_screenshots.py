4810259cb25d24bf0d06caa98cb817365317db3e
Moses <nnythm@gmail.com>
2011-07-24 12:06:39 -0700

    made sample generating script, and script for updating db and pulling images just needs to be updated with width and quality numbers when we choose them.

---
+++ showhn/gen_sample_screenshots.py
@@ -0,0 +1,33 @@
+#
+# Author: MN
+#
+# Generates 100 different files that are all screenshots of google
+# with varying width and quality to compare file sizes
+# takes one argument which is the directory name
+#
+
+import sys
+from screenshot_api import save_file_to_fs
+from screenshot_api import screenshot
+
+QUALITY_INCREMENT = 10
+WIDTH_INCREMENT = 100
+
+dir = sys.argv[1]
+
+
+if dir[-1] is '/':
+    formatted_dir = dir
+else:
+    formatted_dir = dir + '/'
+
+WIDTH_INCREMENT
+
+for outer_loop in range(10):
+
+    for inner_loop in range(10):
+        filename = formatted_dir + "google.com" + str(outer_loop) + str(inner_loop) + '.jpg'
+        quality = inner_loop*QUALITY_INCREMENT
+        width = outer_loop*WIDTH_INCREMENT
+        
+        save_file_to_fs(screenshot("google.com", width, quality), filename)
---
+++ showhn/gen_screenshots.py
@@ -0,0 +1,41 @@
+#
+# Author: MN
+#
+# Iterates over posts in database and makes screenshots for them.
+# saves filename to db once done
+# with varying width and quality to compare file sizes.
+# takes one argument which is the directory name
+#
+
+import sys
+from pymongo import Connection
+from screenshot_api import save_file_to_fs
+from screenshot_api import screenshot
+
+SELECTED_QUALITY = 0
+SELECTED_WIDTH = 0
+
+connection = Connection()
+db = connection['showhn']
+posts_collection = db['posts']
+
+dir = sys.argv[1]
+
+if dir[-1] is '/':
+    formatted_dir = dir
+else:
+    formatted_dir = dir + '/'
+
+cursor = posts_collection.find()
+for post in cursor:
+    if not "filename" in post:
+        url = post['item']['url']
+
+        if url:
+            hnid = post['item']['id']
+            filename = formatted_dir + hnid + '.jpg'
+            quality = SELECTED_QUALITY
+            width = SELECTED_WIDTH
+            save_file_to_fs(screenshot(url, width, quality), filename)
+            post['filename'] = filename
+            posts_collection.save(post)
