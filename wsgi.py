9e51894e642732da02f5749f7b0fb82e5e0fc648
Moses <nnythm@gmail.com>
2011-07-31 13:47:01 -0700

    fixed wsgi.py script

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,12 @@
 import os, sys
 from paste.deploy import loadapp
 current_dir = os.path.dirname(__file__)
-os.symlink("/home/dotcloud/images", "/home/dotcloud/code/showhn/static/images")
-os.symlink("/home/dotcloud/secret.py", "/home/dotcloud/code/showhn/scripts/secret.py")
+
+path = "/home/dotcloud/code/showhn/static/images"
+if not os.path.exists(path):
+    os.symlink("/home/dotcloud/images", path)
+
+path = "/home/dotcloud/code/showhn/scripts/secret.py"
+if not os.path.exists(path):
+    os.symlink("/home/dotcloud/secret.py", path)
 application = loadapp('config:production.ini', relative_to=current_dir)
