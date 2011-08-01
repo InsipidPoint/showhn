d05563d01641906b7e07ebf679744e9f53719d31
Shiwei Song <shiweisong@gmail.com>
2011-08-01 01:34:51 -0700

    adding links to page and post

--- showhn/views.py
+++ showhn/views.py
@@ -20,11 +20,11 @@ def view_hn(request):
     page_start = 0 if 'page' not in request.params else (int(request.params['page'])-1)*12
     search_result = search('"show hn"', page_start, modifier, 12)
     posts = [p['item'] for p in search_result[1]]
-#    pprint(posts)
+    pprint(posts)
     radio = modifier.split()
     fetch_list = [(post["id"], post["url"]) for post in posts]
     images_map = fetch(fetch_list, IMAGE_DIRECTORY)
-    template_map = {'posts':posts, 'create_ts':'', 'points':'', 'num_comments':'', 'asc':'', 'desc':'', 'pages':search_result[0]/12, 'current':page_start/12+1, 'image_map': images_map}
+    template_map = {'posts':posts, 'create_ts':'', 'points':'', 'num_comments':'', 'asc':'', 'desc':'', 'pages':search_result[0]/12 if search_result[0] < 980 else 82, 'current':page_start/12+1, 'image_map': images_map}
     template_map[radio[0]] = 'checked="checked"'
     template_map[radio[1]] = 'checked="checked"'
     return template_map
