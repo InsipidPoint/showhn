48feaf8d34f22dfcf712b15a21bd9421327ff66c
Moses <nnythm@gmail.com>
2011-08-01 22:04:44 -0700

    fixed fetching

--- showhn/scripts/fetch_screenshots.py
+++ showhn/scripts/fetch_screenshots.py
@@ -3,11 +3,13 @@ from pymongo import Connection
 from screenshot_api import save_file_to_fs
 from screenshot_api import screenshot
 from threading import Thread
+from time import time
 
 DUMMY_FILENAME = "static/dummy.png"
 NONE_FILENAME = "static/none.png"
 SELECTED_QUALITY = 70
 SELECTED_WIDTH = 500
+TIMEOUT = 600
 
 #input must be a list of id and url tuples
 #output is a map from id to filename.
@@ -25,27 +27,35 @@ def fetch(ids_and_urls, directory):
     fetcher = Fetcher()
         
     for id_name, url in ids_and_urls:
-        filename = formatted_dir + str(id_name) + '.png'
+        png_filename = formatted_dir + str(id_name) + '.png'
         if url is None:
             ret_dict[id_name] = nonefile
             continue
         try:
-            fp = open(filename)
+            fp = open(png_filename)
+            temp = fp.read()
             fp.close()
-            ret_dict[id_name] = filename
-            found_file = True
+            if len(temp) < 100:
+                found_file = False
+            else:
+                ret_dict[id_name] = png_filename
+                found_file = True
         except:
             found_file = False
 
         if not found_file:
-            filename = formatted_dir + str(id_name) + '.jpg'
+            jpg_filename = formatted_dir + str(id_name) + '.jpg'
             try:
-                fp = open(filename)
+                fp = open(jpg_filename)
                 fp.close()
-                ret_dict[id_name] = filename
+                ret_dict[id_name] = jpg_filename
             except:
+                time_str = str(time())
+                fp = open(png_filename, "w")
+                fp.write(time_str)
+                fp.close()
                 ret_dict[id_name] = dummy
-                to_be_found.append((id_name,url))
+                to_be_found.append((id_name,url,time_str))
 
     thread = Thread(target=fetcher, args = (to_be_found, formatted_dir))
     thread.start()
@@ -54,14 +64,16 @@ def fetch(ids_and_urls, directory):
 
 class Fetcher:
     #requires a list of ids and urls
-    def __call__(self, ids_and_urls, formatted_dir):
-        for hnid, url in ids_and_urls:
+    def __call__(self, ids_and_urls_and_times, formatted_dir):
+        for hnid, url, time_str in ids_and_urls_and_times:
             filename = formatted_dir + str(hnid) + '.png'
             try:
                 fp = open(filename)
+                cur_str = fp.read()
                 fp.close()
+                if cur_str == time_str:
+                    quality = SELECTED_QUALITY
+                    width = SELECTED_WIDTH
+                    save_file_to_fs(screenshot(url), filename)
             except:
-                quality = SELECTED_QUALITY
-                width = SELECTED_WIDTH
-                save_file_to_fs(screenshot(url), filename)
-
+                pass
